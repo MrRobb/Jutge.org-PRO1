d432410ccb2ecb622e758638548458152ac1d1ab
Roberto Ariosa <rob.ariosa@hotmail.com>
2016-10-31 16:36:37 +0100

    Perfect Primes Added

--- Jutge-P5/07 - P22467 PerfectPrimes/main.cpp
+++ Jutge-P5/07 - P22467 PerfectPrimes/main.cpp
@@ -2,20 +2,23 @@
 
 using namespace std;
 
-// NOT FINISHED
+int suma_digitos(int n) {
+  if (n < 10) return n;
+  return n%10 + suma_digitos(n/10);
+}
 
-bool is_perfect_prime(int n){
-  if (n < 10){
-    return (n == 2 or n == 3 or n == 5 or n == 7);
-  } else {
-
+bool is_prime(int n) {
+  if (n <= 1) return false;
+  for (int i = 2; i*i <= n; i++) {
+    if (n%i == 0) return false;
   }
+  return true;
+}
 
+bool is_perfect_prime(int n) {
+  if (n < 10) return is_prime(n);
+  return is_prime(n) and is_perfect_prime(suma_digitos(n));
 }
 
 int main() {
-  int n;
-  cin >> n;
-  cout << is_perfect_prime(n);
-
 }
