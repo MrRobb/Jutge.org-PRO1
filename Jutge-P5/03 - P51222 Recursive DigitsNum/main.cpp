c309eed588403896ae49e7022fa1fa4dcf8e1a7e
Roberto Ariosa <rob.ariosa@hotmail.com>
2016-10-31 15:16:52 +0100

    -

---
+++ Jutge-P5/03 - P51222 Recursive DigitsNum/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int number_of_digits(int n){
+  if (n < 10){
+    return 1;
+  } else {
+    return 1 + number_of_digits(n/10);
+  }
+}
+
+int main() {
+}
