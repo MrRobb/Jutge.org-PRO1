ecfa69cb8dc66925f9e0fea4d039973b6631ed04
Roberto Ariosa <rob.ariosa@hotmail.com>
2016-11-03 20:37:49 +0100

    Base -2
    Fet però… eficiencia es una caca

--- Jutge-P7/08 - P93533 Base-2/main.cpp
+++ Jutge-P7/08 - P93533 Base-2/main.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
 #include <cmath>
 
-// NOT FINISHED (YET)
-
 using namespace std;
 
 int digitos(int n){
@@ -12,14 +10,38 @@ int digitos(int n){
   return digitos;
 }
 
-int base2(){
-  
+int base2(int n, string& cambiado, int weight, int digitos){
+  if (n != 0 and weight <= digitos){
+    if (n%-2 == 0){
+      cambiado += '0';
+      base2(n/-2, cambiado, ++weight, digitos);
+    } else if (n%-2 == 1){
+      cambiado += '1';
+      base2(n/-2, cambiado, ++weight, digitos);
+    } else if (n%-2 == -1){
+      cambiado += '1';
+      base2((n)/(-2) + 1, cambiado, ++weight, digitos);
+    }
+  }
+  return n;
 }
 
 int main() {
 
   int n;
-  cin >> n;
-  cout << base2();
+  while(cin >> n){
+
+    int weight = 0;
+    string cambiado;
+    if (n == 0) cambiado = "0";
+    else {
+      base2(n, cambiado, weight, digitos(n));
+    }
+
+    cout << n << ": ";
+
+    for (int i = int(cambiado.size())-1; i>=0 ; i--) cout << cambiado[i];
+    cout << endl;
+  }
 
 }
