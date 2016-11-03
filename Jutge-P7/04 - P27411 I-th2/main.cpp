f17da8324a892128605f89e2279b9e8b26e043fa
Roberto Ariosa <rob.ariosa@hotmail.com>
2016-11-03 11:39:16 +0100

    I-èsim 2 Added

---
+++ Jutge-P7/04 - P27411 I-th2/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+
+  int n;
+  int valor;
+  int i = 0;
+  vector<int> v;
+  cin >> n;
+  cin >> valor;
+
+  while (valor != -1){
+    v.push_back(valor);
+    cin >> valor;
+    ++i;
+  }
+
+  if (n > 0 and v.size() > n-1) cout << "At the position " << n << " there is a(n) " << v[n-1] << "." << endl;
+  else cout << "Incorrect position." << endl;
+
+
+
+}
