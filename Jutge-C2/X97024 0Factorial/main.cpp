c52f2cfc29e9591f4e5274dd6bc216b053a17fd1
Roberto Ariosa <rob.ariosa@hotmail.com>
2016-11-11 23:13:15 +0100

    0Factorial Added

--- Jutge-C2/X97024 0Factorial/main.cpp
+++ Jutge-C2/X97024 0Factorial/main.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
 int nzeros_fact(int x){
-
+  int sum = 0;
+  for (int i = 1; x > pow(5,i); i++){
+    sum += (x / pow(5,i));
+  }
+  return sum;
 }
-int main() {
 
-
-
-}
+int main() {}
