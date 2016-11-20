1cd5a34b3dffbacc5ff52be0ada3c2c06fc4d2d8
Roberto Ariosa <rob.ariosa@hotmail.com>
2016-11-20 12:10:18 +0100

    :"(

--- Jutge-C2/06 - P66559 TrianglesBufons/main.cpp
+++ Jutge-C2/06 - P66559 TrianglesBufons/main.cpp
@@ -6,7 +6,22 @@ using namespace std;
 // NOT FINISHED
 
 void escriu_triangle(int t){
+  int lineas = t*2;
 
+  for (int i = 1; i <= lineas; ++i){ // Cada linea
+
+    // Poner espacios
+    for(int counter_spaces = lineas-i; counter_spaces >= 0; --counter_spaces){
+      cout << ' ';
+    }
+
+    cout << "/";
+
+
+
+
+    cout << endl;
+  }
 }
 
 int main() {
