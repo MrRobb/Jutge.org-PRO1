6440eb2a563fa0d3985b5348831177c41f7afa85
Roberto's Robots <roberto.ariosa@est.fib.upc.edu>
2016-11-24 10:53:31 +0100

    Creates a Random Matrix

---
+++ Jutge-P9/randomMatrix.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <stdlib.h> 
+#include <vector>
+
+using namespace std;
+typedef vector< vector<int> > Matrix;
+
+Matrix randomMatrix(int filas, int columnas){
+	Matrix m(filas, vector<int>(columnas));
+	for (int i = 0; i < filas; ++i){
+		for(int j = 0; j < columnas; ++j){
+			m[i][j] = rand() % 10;
+		}
+	}
+	return m;
+}
+
+void printMatrix(Matrix m){
+	for (int i = 0; i < m.size(); ++i){
+		for(int j = 0; j < m[0].size(); ++j){
+			cout << m[i][j] << ' ';
+		}
+		cout << endl;
+	}
+}
+
+int main(){
+	Matrix m = randomMatrix(6,6);
+	printMatrix(m);
+	cout << endl;
+}
