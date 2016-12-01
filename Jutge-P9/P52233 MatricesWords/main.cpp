74c399a1d133c890a7d1719b3bf877593d88e112
Roberto Ariosa <rob.ariosa@hotmail.com>
2016-12-01 10:45:59 +0100

    plz

--- Jutge-P9/P52233 MatricesWords/main.cpp
+++ Jutge-P9/P52233 MatricesWords/main.cpp
@@ -9,10 +9,10 @@ typedef vector<Row> Matrix;
 int number_of_words(const Matrix& m, char c, int k){
   int n = 0;
   int rows = m.size();
-  int columnas;
+  int columns;
   for (int i = 0; i < rows; ++i){
-    columnas = m[i].size();
-    for (int j = 0; j < columnas; ++j){
+    columns = m[i].size();
+    for (int j = 0; j < columns; ++j){
       int l = m[i][j].length();
       if (k < l and m[i][j][k] == c) ++n;
     }
