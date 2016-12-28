a1c30b815f2c834f7c105db80f1d97a5006695fd
Roberto Ariosa <rob.ariosa@hotmail.com>
2016-12-28 17:39:07 +0100

    Done

--- Jutge-P11/P89124 SieveEratosthenes.cpp
+++ Jutge-P11/P89124 SieveEratosthenes.cpp
@@ -5,20 +5,20 @@ using namespace std;
 
 int main(){
 
-  int n;
-  while (cin >> n){
-    vector<bool> v(n+1,true);
-    v[0] = false;
-    v[1] = false;
+  vector<bool> v(1000001,true);
+  v[0] = false;
+  v[1] = false;
 
-    for(int i = 2; i*i <= n; ++i){
-      if(v[i] == true){
-        for(int j = 0; i*i+j*i <= n; ++j){
-          v[i*i+j*i] = false;
-        }
+  for(int i = 2; i*i <= 1000001; ++i){
+    if(v[i] == true){
+      for(int j = 0; i*i+j*i <= 1000000; ++j){
+        v[i*i+j*i] = false;
       }
     }
+  }
 
+  int n;
+  while (cin >> n){
     if (v[n] == true) cout << n << " is prime" << endl;
     else cout << n << " is not prime" << endl;
   }
