bb82fbcbd35d7b050041bd9116f747e7e630a583
Roberto Ariosa <rob.ariosa@hotmail.com>
2017-01-05 20:22:15 +0100

    Done

--- Jutge-P11/P77227 Jump.cpp
+++ Jutge-P11/P77227 Jump.cpp
@@ -3,11 +3,22 @@
 //
 
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 string exit(int p, vector<int>& v){
+  int size = v.size();
+  int pini;
+  while(v[p] != 0 and 0 <= p+v[p] and p+v[p] < size){
+    pini = p;
+    p = p+v[p];
+    v[pini] = 0;
+  }
 
+  if(v[p] == 0) return "never";
+  else if(p+v[p] < 0) return "left";
+  else return "right";
 }
 
 int main(){
